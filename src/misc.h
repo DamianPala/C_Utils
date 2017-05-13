9a141031d35795fa3aecb4642dfa52eba4766614
Damian Pala <mail@damianpala.com>
2017-05-13 07:27:39 +0200

    Added macros to build and disassembly word.

--- src/misc.h
+++ src/misc.h
@@ -26,6 +26,13 @@
 /*----------------------------- LOCAL OBJECT-LIKE MACROS -------------------------------*/
 
 /*---------------------------- LOCAL FUNCTION-LIKE MACROS ------------------------------*/
+#define MISC_GET_MSB_BE(word)                       ((word >> 8) & 0xFF)
+#define MISC_GET_LSB_BE(word)                       (word & 0xFF)
+#define MISC_GET_MSB_LE(word)                       (word & 0xFF)
+#define MISC_GET_LSB_LE(word)                       ((word >> 8) & 0xFF)
+#define MISC_BUILD_UINT16(hByte, lByte)             (((uint16_t)hByte << 8) + lByte)
+#define MISC_BUILD_UINT16_BE(rawBytes)               (((uint16_t)rawBytes[0] << 8) + rawBytes[1])
+#define MISC_BUILD_UINT16_LE(rawBytes)               (((uint16_t)rawBytes[1] << 8) + rawBytes[0])
 
 /*======================================================================================*/
 /*                     ####### EXPORTED TYPE DECLARATIONS #######                       */
